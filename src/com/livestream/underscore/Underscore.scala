8d777745631e99ec4f6afee0634bffd564e91c5f
Sourav Chandra <sourav.chandra@livestream.com>
2012-09-03 18:52:03 +0530

    initial commit

---
+++ src/com/livestream/underscore/Underscore.scala
@@ -0,0 +1,61 @@
+package com.livestream.underscore
+
+object Underscore {
+
+  def once(fn: => Any) = {
+    var ran = false
+    var memo: Any = null
+    new Function1[Any, Any] {
+      def apply(v1: Any) = {
+        if (ran) {
+          memo
+        } else {
+          ran = true
+          memo = fn
+          memo
+        }
+      }
+    }
+  }
+  
+  def after(count: Int, fn: => Any) = {
+    new Function1[Any, Any] {
+      var myCount = count
+      def apply(v1: Any) = {
+        myCount = myCount - 1
+        if (myCount < 0) {
+          fn
+        }
+      }
+    }
+  }
+
+  def init(a: Int) = {
+    println("inited : " + a)
+    System.currentTimeMillis()
+  }
+  
+  def render() {
+    println("rendering")
+  }
+  
+  def asyncSave(callback: => Any) {
+    new Thread() {
+      override def run() {
+        println("running callback " + Thread.currentThread())
+        callback
+      }
+    }.start()
+  }
+
+  def main(args: Array[String]) {
+//    var v = once(init(12))
+//    println(v())
+//    println(v())
+//    println(v())
+    var v = after(3, render)
+    for (i <- 1 to 4) {
+      asyncSave(v())
+    }
+  }
+}
