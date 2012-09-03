5b177e31b1b3caa2b22815c5e1841682ced1bc9a
Sourav Chandra <sourav.chandra@livestream.com>
2012-09-03 19:41:17 +0530

    atomic integer

--- src/com/livestream/underscore/Underscore.scala
+++ src/com/livestream/underscore/Underscore.scala
@@ -1,4 +1,5 @@
 package com.livestream.underscore
+import java.util.concurrent.atomic.AtomicInteger
 
 object Underscore {
 
@@ -17,45 +18,44 @@ object Underscore {
       }
     }
   }
-  
+
   def after(count: Int, fn: => Any) = {
     new Function1[Any, Any] {
-      var myCount = count
+      var myCount = new AtomicInteger(count)
       def apply(v1: Any) = {
-        myCount = myCount - 1
-        if (myCount < 0) {
+        if (myCount.decrementAndGet() < 0) {
           fn
         }
       }
     }
   }
 
-//private[this] def init(a: Int) = {
-//  println("inited : " + a)
-//  System.currentTimeMillis()
-//}
-//
-//private[this]def render() {
-//  println("rendering")
-//}
-//
-//private[this] def asyncSave(callback: => Any) {
-//  new Thread() {
-//    override def run() {
-//      println("running callback " + Thread.currentThread())
-//      callback
-//    }
-//  }.start()
-//}
-//
-// def main(args: Array[String]) {
-//    var v = once(init(12))
-//    println(v())
-//    println(v())
-//    println(v())
-//    var v = after(3, render)
-//  for (i <- 1 to 4) {
-//    asyncSave(v())
+//  private[this] def init(a: Int) = {
+//    println("inited : " + a)
+//    System.currentTimeMillis()
 //  }
-//}
+//
+//  private[this] def render() {
+//    println("rendering")
+//  }
+//
+//  private[this] def asyncSave(callback: => Any) {
+//    new Thread() {
+//      override def run() {
+//        println("running callback " + Thread.currentThread())
+//        callback
+//      }
+//    }.start()
+//  }
+//
+//  def main(args: Array[String]) {
+//    var v1 = once(init(12))
+//    println(v1())
+//    println(v1())
+//    println(v1())
+//    var v2 = after(3, render)
+//    for (i <- 1 to 4) {
+//      asyncSave(v2())
+//    }
+//  }
 }
